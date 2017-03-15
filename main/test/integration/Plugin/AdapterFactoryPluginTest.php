61f1ab73d37f0e039fb7a27492d48c38f37bb407
Andreas von Studnitz <avs@integer-net.de>
2017-03-15 16:09:17 +0100

    Add adapter factory plugin integration test

--- main/test/integration/Plugin/AdapterFactoryPluginTest.php
+++ main/test/integration/Plugin/AdapterFactoryPluginTest.php
@@ -13,6 +13,8 @@ use IntegerNet\Solr\Model\Plugin\AdapterFactoryPlugin;
 use Magento\Search\Model\AdapterFactory;
 use Magento\TestFramework\Interception\PluginList;
 use Magento\TestFramework\ObjectManager;
+use IntegerNet\Solr\Model\Search\Adapter\SolrAdapter;
+use Magento\Framework\Search\Adapter\Mysql\Adapter as MysqlAdapter;
 
 class AdapterFactoryPluginTest extends \PHPUnit_Framework_TestCase
 {
@@ -38,5 +40,37 @@ class AdapterFactoryPluginTest extends \PHPUnit_Framework_TestCase
         $this->assertSame(AdapterFactoryPlugin::class, $pluginInfo['integernet_solr_choose_adapter']['instance']);
     }
 
+    /**
+     * @magentoAppArea frontend
+     * @magentoDbIsolation enabled
+     * @magentoAppIsolation enabled
+     * @magentoConfigFixture current_store integernet_solr/general/is_active 0
+     */
+    public function testMysqlAdapterIsCreated()
+    {
+        /** @var AdapterFactory $adapterFactory */
+        $adapterFactory = $this->objectManager->create(AdapterFactory::class);
+        $this->assertInstanceOf(AdapterFactory::class, $adapterFactory);
 
+        $adapter = $adapterFactory->create();
+        $this->assertInstanceOf(MysqlAdapter::class, $adapter);
+    }
+
+    /**
+     * @magentoAppArea frontend
+     * @magentoDbIsolation enabled
+     * @magentoAppIsolation enabled
+     * @magentoConfigFixture current_store integernet_solr/general/is_active 1
+     */
+    public function testSolrAdapterIsCreated()
+    {
+        $this->markTestSkipped('Setting configuration doesn\'t work with Magento 2.1.5.');
+
+        /** @var AdapterFactory $adapterFactory */
+        $adapterFactory = $this->objectManager->create(AdapterFactory::class);
+        $this->assertInstanceOf(AdapterFactory::class, $adapterFactory);
+
+        $adapter = $adapterFactory->create();
+        $this->assertInstanceOf(SolrAdapter::class, $adapter);
+    }
 }
