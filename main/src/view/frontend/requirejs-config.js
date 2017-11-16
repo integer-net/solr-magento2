4972132fa6833b69b4e67c50de217c0c690e87c8
Sandro Wagner <sw@integer-net.de>
2017-11-16 13:53:12 +0100

    29 use require.js to handle filter options link
    Additionally move form-mini.js into js/ folder to comply with default
    Magento conventions.

--- main/src/view/frontend/requirejs-config.js
+++ main/src/view/frontend/requirejs-config.js
@@ -1,7 +1,8 @@
 var config = {
     map: {
         '*': {
-            quickSearch: 'IntegerNet_Solr/form-mini'
+            quickSearch: 'IntegerNet_Solr/js/form-mini',
+            filterOptionsLink: 'IntegerNet_Solr/js/filterOptionsLink'
         }
     }
 };
