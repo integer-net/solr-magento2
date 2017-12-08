e4f0ce4c3c793dc547f49ad183a1d6cc090c1965
Sandro Wagner <sw@integer-net.de>
2017-12-08 16:49:52 +0100

    7455 add jquery-ui slider component as a dummy

--- main/src/view/frontend/requirejs-config.js
+++ main/src/view/frontend/requirejs-config.js
@@ -1,7 +1,8 @@
 var config = {
     map: {
         '*': {
-            quickSearch: 'IntegerNet_Solr/form-mini'
+            quickSearch: 'IntegerNet_Solr/form-mini',
+            jQueryUiSlider: 'IntegerNet_Solr/jquery-ui/jquery-ui-1.10.4.slider.min'
         }
     }
 };
