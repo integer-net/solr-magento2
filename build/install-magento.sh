14767152a2ccc7d5caa0eafaa9a00d385d2e6fbc
Fabian Schmengler <fs@integer-net.de>
2017-12-14 16:29:16 +0100

    Update Wercker configuration
    Squashed commits
    ---
    
    Update wercker.yaml to run all tests
    
    Wercker: Fix path to module in unit test step
    
    Move wercker steps to bash scripts
    
    Also updated Magento version for tests from 2.1.5 to 2.1.11
    
    Fix paths
    
    Use Magento 2.1.10
    
    2.1.11 is not yet available in the nexcess mirror
    
    Wercker: specify fingerprint type for github.com
    
    Wercker: Only run composer update once
    
    Wercker: Add missing variables to integration test script
    
    Wercker: skip module:enable
    
    Wercker: use cache for composer

---
+++ build/install-magento.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -e
+set -x
+echo memory_limit=-1 >> /usr/local/etc/php/php.ini
+git checkout -b tmp
+git add -A
+git commit --allow-empty -m "tmp"
+export MODULE_DIR=`pwd`
+export M2SETUP_DB_HOST=$MYSQL_CI_PORT_3306_TCP_ADDR
+export M2SETUP_DB_USER=root
+export M2SETUP_DB_PASSWORD=$MYSQL_CI_ENV_MYSQL_ROOT_PASSWORD
+export M2SETUP_DB_NAME=magento
+export M2SETUP_BASE_URL=http://m2.localhost:8000/
+export M2SETUP_ADMIN_FIRSTNAME=Admin
+export M2SETUP_ADMIN_LASTNAME=User
+export M2SETUP_ADMIN_EMAIL=dummy@example.com
+export M2SETUP_ADMIN_USER=magento2
+export M2SETUP_ADMIN_PASSWORD=magento2
+export M2SETUP_VERSION=2.1.10
+export M2SETUP_USE_SAMPLE_DATA=false
+export M2SETUP_USE_ARCHIVE=true
+export COMPOSER_HOME=$WERCKER_CACHE_DIR/composer
+mysqladmin -u$M2SETUP_DB_USER -p"$M2SETUP_DB_PASSWORD" -h$M2SETUP_DB_HOST create $M2SETUP_DB_NAME
+/usr/local/bin/mage-setup
+cd /srv/www
+composer config http-basic.repo.magento.com $MAGENTO_REPO_PUBLIC_KEY $MAGENTO_REPO_PRIVATE_KEY
+composer config repositories.solr-module vcs $MODULE_DIR
+composer config repositories.solr-autosuggest vcs git@github.com:integer-net/solr-magento2-autosuggest.git
+composer config repositories.solr-base vcs git@github.com:integer-net/solr-base.git
+composer config repositories.solr-pro vcs git@github.com:integer-net/solr-pro.git
+sed -i -e 's/"psr-4": {/"psr-4": {\n      "IntegerNet\\\\Solr\\\\": ["vendor\/integer-net\/solr-magento2\/main\/test\/unit", "vendor\/integer-net\/solr-magento2\/main\/test\/integration", "vendor\/integer-net\/solr-base\/test\/Solr" ],/g' composer.json
+composer config minimum-stability dev
+composer require integer-net/solr-magento2 dev-tmp --no-update
+composer require --dev phpunit/phpunit ^5.0 --no-update
+composer update
+sed -i -e "s/8983/$SOLR_CI_PORT_8983_TCP_PORT/g"  vendor/integer-net/solr-magento2/main/test/integration/_files/solr_config.dist.php
+sed -i -e "s/localhost/$SOLR_CI_PORT_8983_TCP_ADDR/g" vendor/integer-net/solr-magento2/main/test/integration/_files/solr_config.dist.php
+sed -i -e "s/solr-magento2-tests/core0/g" vendor/integer-net/solr-magento2/main/test/integration/_files/solr_config.dist.php
